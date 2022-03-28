cc8059ba9ba2abd1af43c810342ccc4cacf23c5c
Cesar Avila Frias <cafmacdbit@MacBook-Pro-de-Cesar.local>
2022-03-28 11:29:09 -0300

    Se corrigio la ubicacion de la impresion por pantalla del resultado

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-printf('Tempo en python, iniciando la funcionalidad!')
-
+import sys
 def sumar(a, b):
     result = 0
     result = a + b
     return 'La suma es : {}'.format(result)
+
+printf('Tempo en python, iniciando la funcionalidad! {}', sumar(2+2))
